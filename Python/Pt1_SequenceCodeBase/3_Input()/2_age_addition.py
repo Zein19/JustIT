600906d7a4f8fb4c5154f336da19f59a3b15175b
Zein19 <zein.23@hotmail.co.uk>
2023-12-13 15:34:31 +0000

    python lessons

---
+++ Python/Pt1_SequenceCodeBase/3_Input()/2_age_addition.py
@@ -0,0 +1,36 @@
+# The default data type for the input function is string
+# The input function allow the program to take user input
+"Objectives"
+"" '' # Casting and/or conversion
+
+userAge = int(input(("Please enter your age: ")))
+print(type(userAge))
+print("Your age is: ", userAge)
+
+"To DO: Exercise: Provide screenshot to the anwers to the questions below in the chat"
+
+# To DO:
+# what is the equivalent of input in JavaScript?
+# prompt
+
+# what is the equivalent of converting a string to integer inJavaScript?
+# parseInt
+
+# Format the print statment using f-string
+print(f'Your age is: {userAge}')
+
+
+"To DO: Task 2: Working with the input function and datatypes"
+
+num1 = int(input("Enter your first number: "))
+print(type(num1))
+print(f'Your first number is {num1}')
+# To complete the code block above
+# Use a variable and invoke the input function ask for a second number
+
+# Add the two numbers
+# print out the total of the two numbers
+# Format your print statement to read, The answer to num1Value + num2Value is TotalValue
+
+"To DO: Task 3: Working with the input function and datatypes"
+# Modify you solution above to take in decimal values instead of whole numbers
